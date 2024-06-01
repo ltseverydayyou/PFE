68103cf10dfcf1fef1b02fa24ef6034bc5f468ca
maxype <yepsecretfinder@gmail.com>
2024-06-01 22:29:11 +0300

    Update code.lua

--- scripts/code.lua
+++ scripts/code.lua
@@ -1,1 +1,5 @@
-print'femboy viper'
+workspace.Multiplayer.DescendantAdded:connect(function(lol)
+    if lol:IsA("BasePart") then
+        lol.Color=Color3.fromRGB(math.random(1,255),math.random(1,255),math.random(1,255))
+    end
+end)
