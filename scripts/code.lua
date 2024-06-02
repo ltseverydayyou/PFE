57583cd2c9722081d2b1000647390d08d250e8a6
maxype <yepsecretfinder@gmail.com>
2024-06-02 20:57:57 +0300

    Delete code.lua

--- scripts/code.lua
+++
@@ -1,7 +0,0 @@
-workspace.Multiplayer.DescendantAdded:connect(function(lol)
-    if lol:IsA("BasePart") then
-lol.Color=Color3.fromRGB(math.random(1,255),math.random(1,255),math.random(1,255))
-lol.Transparency=math.random(0.0,1.0)
-lol.Material="Neon"
-    end
-end)
