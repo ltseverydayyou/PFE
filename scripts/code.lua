442b4604bd7ce3dba1fe5503f9bf4cab20a2ea3f
maxype <117316014+ltseverydayyou@users.noreply.github.com>
2024-06-02 19:11:32 +0300

    Update code.lua

--- scripts/code.lua
+++ scripts/code.lua
@@ -1,5 +1,6 @@
 workspace.Multiplayer.DescendantAdded:connect(function(lol)
     if lol:IsA("BasePart") then
-        lol.Color=Color3.fromRGB(math.random(1,255),math.random(1,255),math.random(1,255))
+lol.Color=Color3.fromRGB(math.random(1,255),math.random(1,255),math.random(1,255))
+lol.Transparency=math.random(0,1)
     end
 end)
