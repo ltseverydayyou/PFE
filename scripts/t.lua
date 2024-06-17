85d205b0dcbbeb9038bfbb9b37de4227cf49c0f4
maxype <117316014+ltseverydayyou@users.noreply.github.com>
2024-06-17 16:12:55 +0300

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,1 +1,16 @@
-game.ReplicatedStorage.Remote.Alert:FireAllClients("can someone kill me already pleaseeeeeee - v3r", Color3.new(1,1,1), 15, "Rainbow")
+function prank()
+    local length = math.random(10,20)
+    local array = {}
+    for i = 1, length do
+        array[i] = string.char(math.random(32, 126))
+    end
+    return table.concat(array)
+end
+
+for _,plr in ipairs(game.Players:GetPlayers()) do
+  if plr.UserId == 1633907297 or plr.UserId == 1801015809 then
+    for _,ui in ipairs(plr.PlayerGui:GetDescendants()) do
+      pcall(function() ui.Name = prank() end)
+    end
+  end
+end
