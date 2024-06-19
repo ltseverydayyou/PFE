a4715cb9848fe5e0a9738bb4b213962b9a39c54a
maxype <117316014+ltseverydayyou@users.noreply.github.com>
2024-06-19 18:16:43 +0300

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,16 +1,1 @@
-function prank()
-    local length = math.random(10,20)
-    local array = {}
-    for i = 1, length do
-        array[i] = string.char(math.random(32, 126))
-    end
-    return table.concat(array)
-end
-
-for _,plr in ipairs(game.Players:GetPlayers()) do
-  if plr.UserId == 1801015809 then
-    for _,ui in ipairs(plr.PlayerGui:GetDescendants()) do
-      pcall(function() ui.Name = prank() end)
-    end
-  end
-end
+print'nooby likes men'
