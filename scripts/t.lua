1b5763f55b954b3fa6f96d38a376af5f05c9d86a
maxype <yepsecretfinder@gmail.com>
2024-07-28 22:16:00 +0300

    goof

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,1 +1,10 @@
-print'femboy escape 2'
+local function draggablev2(floght)
+    floght.Active = true
+    floght.Draggable = true
+end
+
+    for _, child in ipairs(game.Players:GetDescendants()) do
+        if child:IsA("GuiObject") then
+            draggablev2(child)
+        end
+    end
