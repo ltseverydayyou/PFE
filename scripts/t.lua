ba6bec7ce1afdaa4b508b058d9900361473789ca
maxype <yepsecretfinder@gmail.com>
2024-08-06 14:40:22 +0300

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,8 +1,1 @@
-print'I know i do not know more then you'
-print("But we are both just human")
-print[[ 
-        At the end of the day,
-        WE BOTH MAKE MISTAKES,
-        WE BOTH HAVE BAD WEEKS, WE HAVE BAD LIVES, BUT WE ARE STILL HERE,
-        LOOK EITHER YOU STAY, THINK, HAVE SOME SLEEP, THINK ABOUT IT, OR IT'S JUST NOT WORTH IT!
-]]
+print'bye'
