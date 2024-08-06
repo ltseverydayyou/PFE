3d52e165028276d2521dd367be5649da287ba03d
maxype <yepsecretfinder@gmail.com>
2024-08-06 13:44:57 +0300

    bye

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,1 +1,5 @@
-print'no'
+game.Players.PlayerAdded:Connect(function(v)
+    task.wait(math.random(1,25));
+    rem=game:GetService("ReplicatedStorage").Remote.Alert
+    rem:FireClient(v,"It's my time to attempt my suicidal shit and end all of this once and for all - maxype", Color3.new(1,0,0),20)
+end)
