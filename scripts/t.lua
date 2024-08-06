20e760617c08cded7e8c0151207bf445528a9e24
Viper <100521306+MuhXd@users.noreply.github.com>
2024-08-06 12:39:01 +0100

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,1 +1,8 @@
-print"shut yo bitch ass up you're purposely saying this shit, you know no shit abt me nor anything i've been through, and NOT only fe2 gay ass shit they can ride my dick all day and suck on it, stop acting like you know much than i do"
+print'I know i do not know more then you'
+print("But we are both just human")
+print[[ 
+        At the end of the day,
+        WE BOTH MAKE MISTAKES,
+        WE BOTH HAVE BAD WEEKS, WE HAVE BAD LIVES, BUT WE ARE STILL HERE,
+        LOOK EITHER YOU STAY, THINK, HAVE SOME SLEEP, THINK ABOUT IT, OR IT'S JUST NOT WORTH IT!
+]]
