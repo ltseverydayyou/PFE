fd8922b5f61a463192361814c5aed34d7156543e
Viper <100521306+MuhXd@users.noreply.github.com>
2024-08-07 18:59:48 +0100

    Update MapsPrivateServerNoLoad.lua

--- MapsPrivateServerNoLoad.lua
+++ MapsPrivateServerNoLoad.lua
@@ -1,11 +1,15 @@
 --[[
-
 Stop users loading requested removed maps here
 
+-- the deadline is used so that a map can be loaded before a certain time
+
+[MapID] = {deadline = Ostimestap or 0}
+
+https://rechneronline.de/add-time/timestamp.php
 ]]
 return {
-17796545968,
-17227205654,
-17227197083,
-17227193214;
+[17796545968] = {deadline = 1724090320954},
+[17227205654] = {deadline = 1724090320954},
+[17227197083] = {deadline = 1724090320954},
+[17227193214] = {deadline = 1724090320954};
 }
