98f7ef40598a3ab49a0258634abf03c4ad0d1541
maxype <yepsecretfinder@gmail.com>
2024-08-11 21:04:48 +0300

    Create Admins.lua

---
+++ Admins.lua
@@ -0,0 +1,13 @@
+return {
+	[156256804] = true;    -- maxype (idiot/useless)
+	[229501685] = true;    -- mixalispantidos12
+	[5117870034] = true;   -- TotallyNotAwesomee
+	[530829101] = true;    -- momogreek2010
+	[518070034] = true;    -- Aotu4 (LordVery lol)
+	[1801015809] = true;   -- AmaezingNooby
+	[728565053] = true;    -- 4d_el
+	[1633907297] = true;   -- SecretSeason4
+	[1109412317] = true;   -- Vinca
+	[1537680290] = true;   -- ProGamerFe2MapTest
+	[144324719] = true;     -- Cosmic
+}
