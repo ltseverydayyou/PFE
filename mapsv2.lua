a5e26aa279cc5cabdf1e86f5f967dd4888adb044
Izqkus <rie59436@gmail.com>
2025-08-20 21:41:18 -0400

    Update mapsv2.lua

--- mapsv2.lua
+++ mapsv2.lua
@@ -294,7 +294,7 @@ return {
 	[125619079331130] = {Devs = {533717148}, Verified = false, NewMap = true},
 	[97960659725506] = {Devs = {520289047,418823949,278942959,211015031,30102,289258366,1328081083}, Verified = false, NewMap = true},
 	[114316717369574] = {Devs = {552033391}, Verified = false, NewMap = true},
-	[136767191524412] = {Devs = {409264282,1226754577}, Verified = false, NewMap = true},
+	[136767191524412] = {Devs = {409264282,1226754577}, Verified = false, NewMap = true, Pitch=1.2},
 	[127734650495623] = {Devs = {434755962}, Verified = false, NewMap = true},
 	[83781547200025] = {Devs = {138800590,71635576}, Verified = false, NewMap = true},
 	[86457371136611] = {Devs = {409264282}, Verified = false, NewMap = true},
